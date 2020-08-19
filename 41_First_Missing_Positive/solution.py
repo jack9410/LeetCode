4893eb4e101fe47c0727042913a0d5fb151d98c7
jack9410 <46255505+jack9410@users.noreply.github.com>
2020-08-19 18:34:16 +0900

    리트코드 41 풀이

---
+++ 41_First_Missing_Positive/solution.py
@@ -0,0 +1,13 @@
+# List = [1, 2, 0]
+# List = [3,4,-1,1]
+List = [7,8,9,11,12]
+
+minimum = 1
+
+while True:
+    if minimum in List:
+        minimum += 1
+    else:
+        break
+print(minimum)
+
---
+++ 557_Reverse_Words_in_a_String_3/solution.py
@@ -0,0 +1,4 @@
+# 미완
+s = "Let's take LeetCode contest"
+str_lst = s.split('')
+print(str_lst)
